a4bd44bb693ac078a0311195a2545b7c0b4659fb
MOhamed ELfeky <mohamedelfeky202020@gmail.com>
2021-09-09 06:48:22 +0200

    loading images

--- Dino Runner.py
+++ Dino Runner.py
@@ -1,1 +1,20 @@
-#hello World
+import pygame
+
+pygame.init()
+
+#Window parameters
+Height = 600
+Width = 1100
+Screen = pygame.display.set_mode((Width,Height))
+
+#load images
+running = [pygame.image.load("DinoRun1.png"),pygame.image.load("DinoRun2.png")]
+jumping = [pygame.image.load("DinoJump.png")]
+ducking = [pygame.image.load("DinoDuck1.png"),pygame.image.load("DinoDuck2.png")]
+small_cactus = [pygame.image.load("SmallCactus1.png"),pygame.image.load("SmallCactus2.png"),pygame.image.load("SmallCactus3.png")]
+large_cactus = [pygame.image.load("LargeCactus1.png"),pygame.image.load("LargeCactus2.png"),pygame.image.load("LargeCactus3.png")]
+bird = [pygame.image.load("Bird1.png"),pygame.image.load("Bird2.png")]
+cloud = pygame.image.load("Cloud.png")
+BG = pygame.image.load("Track.png")
+
+
