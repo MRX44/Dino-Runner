f0d346fe8d9dbdbda278e21fcdcfc1537ae4f02e
MOhamed ELfeky <mohamedelfeky202020@gmail.com>
2021-09-09 08:06:44 +0200

    initiating class parameters

--- Dino Runner.py
+++ Dino Runner.py
@@ -5,7 +5,8 @@ pygame.init()
 #Window parameters
 Height = 600
 Width = 1100
-Screen = pygame.display.set_mode((Width,Height))
+screen = pygame.display.set_mode((Width,Height))
+white = (255,255,255)
 
 #load images
 running = [pygame.image.load("DinoRun1.png"),pygame.image.load("DinoRun2.png")]
@@ -18,3 +19,45 @@ cloud = pygame.image.load("Cloud.png")
 BG = pygame.image.load("Track.png")
 
 
+class Dinosaur():
+    x_pos = 80 
+    y_pos = 310
+
+    def __init__(self):
+        self.duck_img = ducking
+        self.run_img = running
+        self.jump_img = jumping
+
+        self.dino_run = True
+        self.dino_duck = False
+        self.dino_jump = False
+
+        self.step_index = 0
+        self.image = self.run_img[0]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.x_pos
+        self.dino_rect.y = self.y_pos
+
+        
+
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+    player = Dinosaur()
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        screen.fill(white)
+        userinput = pygame.key.get_pressed()
+
+        
+
+
+        clock.tick(30)
+        pygame.display.update()
+
+main()
