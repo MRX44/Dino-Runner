0ebf313396259c6f915dc29eda4724a82754e07a
MOhamed ELfeky <mohamedelfeky202020@gmail.com>
2021-09-15 11:32:15 +0200

    adding score

--- Dino Runner.py
+++ Dino Runner.py
@@ -8,6 +8,8 @@ Height = 600
 Width = 1100
 screen = pygame.display.set_mode((Width,Height))
 white = (255,255,255)
+black = (0,0,0)
+points = 0
 
 #load images
 running = [pygame.image.load("DinoRun1.png"),pygame.image.load("DinoRun2.png")]
@@ -17,7 +19,7 @@ small_cactus = [pygame.image.load("SmallCactus1.png"),pygame.image.load("SmallCa
 large_cactus = [pygame.image.load("LargeCactus1.png"),pygame.image.load("LargeCactus2.png"),pygame.image.load("LargeCactus3.png")]
 bird = [pygame.image.load("Bird1.png"),pygame.image.load("Bird2.png")]
 cloudy = pygame.image.load("Cloud.png")
-BG = pygame.image.load("Track.png")
+bg = pygame.image.load("Track.png")
 
 
 class Dinosaur():
@@ -116,12 +118,34 @@ class cloud:
     
 def main():
     run = True
-    global game_speed
+    global game_speed , x_pos_bg , y_pos_bg
     game_speed=15
+    x_pos_bg , y_pos_bg = 0,380
     clock = pygame.time.Clock()
     player = Dinosaur()
     Cloud = cloud()
+    points = 0
+    font = pygame.font.Font("freesansbold.ttf",20)
 
+    def score():
+        global points , game_speed
+        points += 1
+        if points % 100 == 0:
+            game_speed +=  1
+        text = font.render("points: " + str(points),True,black)
+        text_rect = text.get_rect()
+
+        screen.blit(text,text_rect)
+
+    def background():
+        global game_speed , x_pos_bg , y_pos_bg
+        image_width = bg.get_width()
+        screen.blit(bg,(x_pos_bg,y_pos_bg))
+        screen.blit(bg,(x_pos_bg+image_width,y_pos_bg)) 
+        if x_pos_bg < -image_width:
+            screen.blit(bg,(x_pos_bg+image_width,y_pos_bg))
+            x_pos_bg  = 0
+        x_pos_bg -= game_speed
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -135,10 +159,13 @@ def main():
         player.draw(screen)
         player.update(userinput)
 
+        background()
+
         Cloud.draw(screen)
         Cloud.update()
-        
 
+        score()
+        
         clock.tick(30)
         pygame.display.update()
 
